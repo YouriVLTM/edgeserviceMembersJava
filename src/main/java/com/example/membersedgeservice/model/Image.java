ed8a2f0afb65c93f4f354e0a4e104ed135d4721d
Youri Van Laer <info@yourivanlaer.be>
2020-12-03 12:09:25 +0100

    Add Image model

---
+++ src/main/java/com/example/membersedgeservice/model/Image.java
@@ -0,0 +1,55 @@
+package com.example.membersedgeservice.model;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+import java.util.Date;
+
+public class Image {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private String id;
+    private String source;
+    private String userEmail;
+    private String description;
+    @Column(unique=true)
+    private String key;
+
+    public Image(String source, String userEmail, String description) {
+        this.source = source;
+        this.description = description;
+        this.userEmail = userEmail;
+        this.key = DigestUtils.sha256Hex(source + userEmail + new Date(System.currentTimeMillis()));
+    }
+
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
