bed18675ad4257179bbfd378759274e3373435d2
Robin Thoelen <rob.thoelen@gmail.com>
2020-12-27 00:15:38 +0100

    updated models and controller for likes

--- src/main/java/com/example/membersedgeservice/controller/LikeController.java
+++ src/main/java/com/example/membersedgeservice/controller/LikeController.java
@@ -20,17 +20,73 @@ public class LikeController {
     private String likeServiceBaseUrl;
 
     @GetMapping("/likes/images/{key}")
-    public List<Like> getLikesByImageId(@PathVariable String key){
+    public List<ImageLike> getLikesByImageId(@PathVariable String key){
 
         // does image exist?
 
-
         // get likes by image
-        ResponseEntity<List<Like>> responseEntityReviews =
+        ResponseEntity<List<ImageLike>> responseEntityReviews =
                 restTemplate.exchange("http://" + likeServiceBaseUrl + "/likes/images/{key}",
-                        HttpMethod.GET, null, new ParameterizedTypeReference<List<Like>>() {
+                        HttpMethod.GET, null, new ParameterizedTypeReference<List<ImageLike>>() {
                         }, key);
 
         return responseEntityReviews.getBody();
+    }
+
+    @PostMapping("/likes")
+    public FilledImageUserLike addLike(@RequestParam String userEmail,
+                                             @RequestParam String imageKey,
+                                             @RequestParam boolean state){
+
+        //TODO check if Userkey exist
+        User user = new User(userEmail);
+
+
+        //TODO check if imageKey exist
+        Image image = new Image();
+        image.setKey(imageKey);
+
+
+        //Make a new like
+        ImageLike like =
+                restTemplate.postForObject("http://" + likeServiceBaseUrl + "/likes",
+                        new ImageLike(state,userEmail,imageKey),ImageLike.class);
+
+        return new FilledImageUserLike(image, user, like);
+    }
+
+    @PutMapping("/likes")
+    public FilledImageUserLike updateLike(@RequestParam String likeKey,
+                                                @RequestParam boolean state) {
+
+        //check if likeKey exist
+        ImageLike like = restTemplate.getForObject("http://" + likeServiceBaseUrl + "/likes/" + likeKey,
+                ImageLike.class);
+
+        // Update like
+        like.setState(state);
+        ResponseEntity<ImageLike> responseEntityReview =
+                restTemplate.exchange("http://" + likeServiceBaseUrl + "/likes",
+                        HttpMethod.PUT, new HttpEntity<>(like), ImageLike.class);
+
+        ImageLike retrievedLike = responseEntityReview.getBody();
+
+        //TODO get user
+        User user = new User();
+
+        //TODO get Images
+        Image image = new Image();
+
+
+        return new FilledImageUserLike(image, user, retrievedLike);
+
+    }
+
+    @DeleteMapping("/likes/{key}")
+    public ResponseEntity deleteLike(@PathVariable String key){
+
+        restTemplate.delete("http://" + likeServiceBaseUrl + "/likes/" + key);
+
+        return ResponseEntity.ok().build();
     }
 }
---
+++ src/main/java/com/example/membersedgeservice/model/FilledImageUserLike.java
@@ -0,0 +1,56 @@
+package com.example.membersedgeservice.model;
+
+public class FilledImageUserLike {
+    private User user;
+    private Image image;
+    private boolean state;
+    private String likeKey;
+
+    public FilledImageUserLike() {
+    }
+
+    public FilledImageUserLike(Image image,User user,ImageLike like) {
+        this.user = new User(user.getUserEmail());
+        this.image = new Image(image.getSource(), image.getUserEmail(), image.getDescription(), image.getKey());
+        this.state = like.getState();
+    }
+
+    public FilledImageUserLike(Image image, User user, boolean state, String likeKey) {
+        this.image = image;
+        this.user = user;
+        this.state = state;
+        this.likeKey = likeKey;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public boolean getState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    public String getLikeKey() {
+        return likeKey;
+    }
+
+    public void setLikeKey(String likeKey) {
+        this.likeKey = likeKey;
+    }
+}
